76a6916b21298741b5e02537efd7ee2177d4cbcc
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-09-09 19:13:47 -0300

    Create seconds private method in Microwave
    Because:
    - It is a food practice keep only one point of access to a attribute
    
    Microwave will abstract the seconds, so keep seconds private is good to better to
    keep the Class API simple.

--- microwave.rb
+++ microwave.rb
@@ -4,7 +4,11 @@ class Microwave
   end
 
   def timer
-    seconds_timer = @seconds.to_s.rjust(2, '0')
+    seconds_timer = seconds.to_s.rjust(2, '0')
     "00:#{seconds_timer}"
   end
+
+  private
+
+  attr_reader :seconds
 end
