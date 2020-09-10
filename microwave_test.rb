5cef843b797d807e24cc111afe3091309afe1f04
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-09-10 17:32:26 -0300

    fixup! fixup! Rename arguments from Microwave#initialize

--- microwave_test.rb
+++ microwave_test.rb
@@ -28,22 +28,18 @@ class MicrowaveTest < Minitest::Test
   end
 
   def test_sixty_one_seconds
-    skip
     assert_equal '01:01', Microwave.new(61).timer
   end
 
   def test_one_minute_and_fifty_nine_seconds
-    skip
     assert_equal '01:59', Microwave.new(159).timer
   end
 
   def test_two_minutes
-    skip
     assert_equal '02:00', Microwave.new(200).timer
   end
 
   def test_over_ten_minutes
-    skip
     assert_equal '10:01', Microwave.new(1001).timer
   end
 end
